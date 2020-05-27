5aa0f7732f38269edd186633d0864660c6620d6b
ApppleTurnover <apppleturnover@gmail.com>
2020-05-28 08:59:46 +1000

    First page

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+import datetime
+import os
+
+from flask import Flask, render_template, redirect, flash, url_for
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = os.urandom(12)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
-    return render_template("index.html", active="index")
+
+    first_time = datetime.datetime(2019, 6, 28)
+    current_time = datetime.datetime.now()
+    work_experience = current_time - first_time
+
+    return render_template("index.html", active="index", time=work_experience)
+
 
 if __name__ == "__main__":
     app.run()
